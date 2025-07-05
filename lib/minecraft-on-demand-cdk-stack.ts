23bfa72865361214d97f2b4125a5e81e5979141a
Victor Fonseca Vega <vrfvega@gmail.com>
2025-07-05 12:50:12 -0400

    add api gateway and provisioning history table

--- lib/minecraft-on-demand-cdk-stack.ts
+++ lib/minecraft-on-demand-cdk-stack.ts
@@ -1,8 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {RemovalPolicy} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {Cors, RestApi} from "aws-cdk-lib/aws-apigateway";
+import {AttributeType, Billing, TableV2} from "aws-cdk-lib/aws-dynamodb";
+
 
 export class MinecraftOnDemandCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const api = new RestApi(this, "MinecraftOnDemandApi", {
+      restApiName: "Minecraft On Demand API",
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: ["GET", "POST"],
+      }
+    });
+
+    const provisioningHistory = new TableV2(this, "ProvisioningHistory", {
+      tableName: "ProvisioningHistory",
+      partitionKey: { name: "executionId", type: AttributeType.STRING},
+      sortKey: { name: "timestamp", type: AttributeType.NUMBER},
+      billing: Billing.onDemand(),
+      deletionProtection: true,
+      removalPolicy: RemovalPolicy.RETAIN
+    })
   }
 }
